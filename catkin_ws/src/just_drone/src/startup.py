f0a9722cec6554e2169c974b906d57b1a7484da7
Anna David <95594851+annadavid1@users.noreply.github.com>
2022-08-03 15:26:31 -0400

    Update startup.py

--- catkin_ws/src/just_drone/src/startup.py
+++ catkin_ws/src/just_drone/src/startup.py
@@ -35,30 +35,7 @@ if __name__ == '__main__':
                 break
             except ValueError:
                 print("Error: Please input a number.")
-
-        # while True:
-        #     i = tv_dim.find("(")
-        #     if i >= 0:
-        #         tv_dim = tv_dim[i+1:]
-        #     i = tv_dim.find(")")
-        #     if i >= 0:
-        #         tv_dim = tv_dim[:i]
-        #     i = tv_dim.find(" ")
-        #     while i >= 0:
-        #         tv_dim = tv_dim[:i] + tv_dim[i+1:]
-        #         i = tv_dim.find(" ")
-        #     try:
-        #         i = tv_dim.index(",")
-        #         w = tv_dim[:i]
-        #         h = tv_dim[i+1:]
-        #         w = int(w)
-        #         h = int(h)
-        #         print("Dimensions received.\n")
-        #         break
-        #     except:
-        #         print("Invalid dimensions. Try again.")
-        #         tv_dim = input("\nInput TV dimensions in inches (w,h): ")
-        
+              
         sound = True # delete when adding sound takeoff
         while not rospy.is_shutdown():
             if sound: # check for start sound
